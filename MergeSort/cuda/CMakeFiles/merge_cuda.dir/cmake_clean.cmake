47e95b298ef219ebf3dcf1feb7616d90b65b73e3
Miguel Garcia <miguelgi347@login2.cluster>
2023-11-03 09:28:44 -0500

    helper fucntion needs revision

---
+++ MergeSort/cuda/CMakeFiles/merge_cuda.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/merge_cuda.dir/merge_cuda.cu.o"
+  "CMakeFiles/merge_cuda.dir/cmake_device_link.o"
+  "merge_cuda.pdb"
+  "merge_cuda"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/merge_cuda.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
