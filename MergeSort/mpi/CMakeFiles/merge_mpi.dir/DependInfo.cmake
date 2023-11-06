3ee913650d4fa1e659f4a075a6b29af040bdff1f
Miguel Garcia <miguelgi347@tamu.edu>
2023-11-06 00:14:53 -0600

    mpi poop

---
+++ MergeSort/mpi/CMakeFiles/merge_mpi.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/miguelgi347/Final_project/CSCE435GroupProject/Utils/helper_functions.cpp" "/home/miguelgi347/Final_project/CSCE435GroupProject/MergeSort/mpi/CMakeFiles/merge_mpi.dir/home/miguelgi347/Final_project/CSCE435GroupProject/Utils/helper_functions.cpp.o"
+  "/home/miguelgi347/Final_project/CSCE435GroupProject/MergeSort/mpi/merge_mpi.cpp" "/home/miguelgi347/Final_project/CSCE435GroupProject/MergeSort/mpi/CMakeFiles/merge_mpi.dir/merge_mpi.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../Utils"
+  "/scratch/group/csce435-f23/Adiak/adiak/include"
+  "/sw/eb/sw/impi/2019.9.304-iccifort-2020.4.304/intel64/include"
+  "/scratch/group/csce435-f23/Caliper/caliper/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
