be02b55923f8576da7b68be8079fbff0a2b8721b
josephquismorio <josephquismorio@gmail.com>
2023-11-08 13:21:03 -0600

    Added CALI timers

--- OddEvenSort/mpi/bubble_mpi.cpp
+++ OddEvenSort/mpi/bubble_mpi.cpp
@@ -9,6 +9,14 @@
 #include <adiak.hpp>
 #include "../../Utils/helper_functions.h"
 
+const char* main_function = "main";
+const char* data_init = "data_init";
+const char* comm = "comm";
+const char* comm_large = "comm_large";
+const char* comp = "comp";
+const char* comp_large = "comp_large";
+const char* correctness_check = "correctness_check";
+
 int compare (const void * a, const void * b)
 {
     float fa = *(const float*) a;
@@ -46,6 +54,9 @@ void localBubbleSort(float *values, int size) {
 void bubbleSort(float *values, int local_data_size, int numTasks, int rankid) {
     float *temp = (float*)malloc(local_data_size * sizeof(float));
 
+    CALI_MARK_BEGIN(comp);
+    CALI_MARK_BEGIN(comp_large);
+
     for (int phase = 0; phase < numTasks; phase++) {
         MPI_Status status;
         
@@ -57,12 +68,24 @@ void bubbleSort(float *values, int local_data_size, int numTasks, int rankid) {
             neighbor = (rankid % 2 != 0) ? rankid + 1 : rankid - 1;
         }
 
+        CALI_MARK_END(comp_large);
+    	CALI_MARK_END(comp);
+
+        CALI_MARK_BEGIN(comm);
+    	CALI_MARK_BEGIN(comm_large);
+
         // Avoid out-of-bounds ranks
         if (neighbor >= 0 && neighbor < numTasks) {
             // Send and receive values
             MPI_Sendrecv(values, local_data_size, MPI_FLOAT, neighbor, 0,
                          temp, local_data_size, MPI_FLOAT, neighbor, 0,
                          MPI_COMM_WORLD, &status);
+
+            CALI_MARK_END(comm_large);
+    	    CALI_MARK_END(comm);
+
+            CALI_MARK_BEGIN(comp);
+            CALI_MARK_BEGIN(comp_large);
 
             // Merge two sorted blocks: values and temp
             if (rankid < neighbor) {
@@ -78,6 +101,9 @@ void bubbleSort(float *values, int local_data_size, int numTasks, int rankid) {
             }
         }
     }
+    
+    CALI_MARK_END(comp_large);
+    CALI_MARK_END(comp);
 
     free(temp);
 }
@@ -107,13 +133,17 @@ int main(int argc, char** argv) {
         MPI_Abort(MPI_COMM_WORLD, rc);
         exit(1);
     }
+
+    CALI_MARK_BEGIN(main_function);
     
     int local_data_size = data_size / numTasks;
     float *values = (float*)malloc(local_data_size * sizeof(float));
 
+    CALI_MARK_BEGIN(data_init);
     array_fill_random_no_seed(values, local_data_size);
+    CALI_MARK_END(data_init);
 
-    localBubbleSort(values, local_data_size);
+    // localBubbleSort(values, local_data_size);
 
     bubbleSort(values, local_data_size, numTasks, rankid);
 
@@ -132,6 +162,8 @@ int main(int argc, char** argv) {
 
     free(values);
     free(global_array);
+
+    CALI_MARK_END(main_function);
 
     MPI_Finalize();
 
