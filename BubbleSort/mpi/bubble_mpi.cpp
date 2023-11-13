523938b8693e76e2a0708c2626073b8d3447d0a5
Joseph Francis Bejar Quismorio <jfquismorio@login3.cluster>
2023-11-13 13:09:17 -0600

    Adiak fixes, going to parameterize soon

--- BubbleSort/mpi/bubble_mpi.cpp
+++ BubbleSort/mpi/bubble_mpi.cpp
@@ -10,7 +10,6 @@
 #include <adiak.hpp>
 #include "../../Utils/helper_functions.h"
 
-const char* main_function = "main";
 const char* data_init = "data_init";
 const char* comm = "comm";
 const char* comm_large = "comm_large";
@@ -116,7 +115,7 @@ void bubbleSort(float *values, int local_data_size, int numTasks, int rankid) {
 }
 
 int main(int argc, char** argv) {
-    // CALI_CXX_MARK_FUNCTION;
+    CALI_CXX_MARK_FUNCTION;
 
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <num_values> <num_processes>\n", argv[0]);
@@ -128,11 +127,11 @@ int main(int argc, char** argv) {
         rankid,
         rc;
 
-    float *global_array = nullptr;
+    float *global_array = (float*)malloc(data_size * sizeof(float));
 
-    if (rankid == 0) {
-        global_array = (float*)malloc(data_size * sizeof(float));
-    }
+    // if (rankid == 0) {
+    //     global_array = (float*)malloc(data_size * sizeof(float));
+    // }
 
     MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD,&rankid);
@@ -143,8 +142,6 @@ int main(int argc, char** argv) {
         MPI_Abort(MPI_COMM_WORLD, rc);
         exit(1);
     }
-
-    CALI_MARK_BEGIN(main_function);
     
     int local_data_size = data_size / numTasks;
     float *values = (float*)malloc(local_data_size * sizeof(float));
@@ -162,7 +159,7 @@ int main(int argc, char** argv) {
     // print_array(global_array, data_size);
 
     CALI_MARK_BEGIN(correctness_check);
-    
+
     if (rankid == 0) {
         bool correct = check_sorted(global_array, data_size);
         if (correct) {
@@ -177,7 +174,36 @@ int main(int argc, char** argv) {
     free(values);
     free(global_array);
 
-    CALI_MARK_END(main_function);
+    if(rankid == 0){
+        const char* algorithm = "Bubble sort";
+		const char* programmingModel = "MPI";
+		const char* datatype = "float";
+		int sizeOfDatatype = sizeof(float);
+		int inputSize = data_size;
+		const char* inputType = "Random";
+		int num_procs = numTasks;
+		const char* num_threads = "N/A";
+		const char* num_blocks = "N/A";
+		int group_number = 1;
+		const char* implementation_source = "Online/AI";
+
+		adiak::init(NULL);
+		adiak::launchdate();    // launch date of the job
+		adiak::libraries();     // Libraries used
+		adiak::cmdline();       // Command line used to launch the job
+		adiak::clustername();   // Name of the cluster
+		adiak::value("Algorithm", algorithm); // The name of the algorithm you are using (e.g., "MergeSort", "BitonicSort")
+		adiak::value("ProgrammingModel", programmingModel); // e.g., "MPI", "CUDA", "MPIwithCUDA"
+		adiak::value("Datatype", datatype); // The datatype of input elements (e.g., double, int, float)
+		adiak::value("SizeOfDatatype", sizeOfDatatype); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+		adiak::value("InputSize", inputSize); // The number of elements in input dataset (1000)
+		adiak::value("InputType", inputType); // For sorting, this would be "Sorted", "ReverseSorted", "Random", "1%perturbed"
+		adiak::value("num_procs", num_procs); // The number of processors (MPI ranks)
+		adiak::value("num_threads", num_threads); // The number of CUDA or OpenMP threads
+		adiak::value("num_blocks", num_blocks); // The number of CUDA blocks 
+		adiak::value("group_num", group_number); // The number of your group (integer, e.g., 1, 10)
+		adiak::value("implementation_source", implementation_source); // Where you got the source code of your algorithm; choices: ("Online", "AI", "Handwritten").
+    }
 
     MPI_Finalize();
 
