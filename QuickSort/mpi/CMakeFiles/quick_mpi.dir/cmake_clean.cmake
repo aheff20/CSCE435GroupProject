c832f29217e42d603a2bd83cd73c5b6e1a8da09b
Miguel Garcia <miguelgi347@tamu.edu>
2023-11-14 11:13:29 -0600

    hi

---
+++ QuickSort/mpi/CMakeFiles/quick_mpi.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quick_mpi.dir/quick_mpi.cpp.o"
+  "CMakeFiles/quick_mpi.dir/home/miguelgi347/Final_project/CSCE435GroupProject/Utils/helper_functions.cpp.o"
+  "quick_mpi.pdb"
+  "quick_mpi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quick_mpi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
