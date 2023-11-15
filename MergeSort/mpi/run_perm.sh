2326fad516332ea7ecbdd8a2734534cf5220c429
Miguel Garcia <miguelgi347@tamu.edu>
2023-11-15 15:47:18 -0600

    mpi cali into out folder

--- MergeSort/mpi/run_perm.sh
+++ MergeSort/mpi/run_perm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Array of input sizes (2^16, 2^18, ..., 2^28)  65536 262144 1048576 4194304 16777216 67108864 268435456           
-inputSizes=( 1048576 16777216   )
+inputSizes=(   1048576 4194304    )
 
 # Array of proc  (2 4 8 16 32 64 128 256 512 1024 )  try 256 on 4 nodes
-procs=( 256  )
+procs=( 128 )
 
 methods=('s' 'r' 'a' 'p')
 
