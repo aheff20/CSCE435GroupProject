477e57113c6aa8951cdbd3f7fba1282c18e08996
Miguel Garcia <miguelgi347@tamu.edu>
2023-11-28 14:33:10 -0600

    merge conflict

--- MergeSort/mpi/run_perm.sh
+++ MergeSort/mpi/run_perm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Array of input sizes (2^16, 2^18, ..., 2^28)  65536 262144 1048576 4194304 16777216 67108864 268435456           
-inputSizes=(  65536	262144	1048576	4194304	16777216	67108864	268435456   )
+# Array of input sizes (2^16, 2^18, ..., 2^28)  65536 262144 1048576 4194304 16777216 67108864 268435456           65536	262144	1048576	4194304	16777216	67108864	268435456 
+inputSizes=(  	65536	262144	1048576	4194304	16777216	67108864   )
 
 # Array of proc  (2 4 8 16 32 64 128 256 512 1024 )  try 256 on 4 nodes.  'r' 'a' 'p'. 256 128. 's'.       4 8 16 32 64 128
-procs=( 2  )
+procs=( 128  )
 
-methods=('a' )
+methods=('p' )
 
 # Iterate over each input size
 for size in "${inputSizes[@]}"; do
@@ -14,7 +14,7 @@ for size in "${inputSizes[@]}"; do
     # for procs in "${procs[@]}"; do
     for method in "${methods[@]}"; do
     # Execute the command with the current size and thread count
-        sbatch mpi_merge.grace_job $size 2 $method
+        sbatch mpi_merge.grace_job $size 128 $method
     done
     # done
 done
